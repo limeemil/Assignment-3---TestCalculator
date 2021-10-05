f8715da86deb82f2a52066cc8441cd93d558b91a
Andreas Lönnermark <andreas.lonnermark@gmail.com>
2021-10-05 09:27:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_3___TestCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
